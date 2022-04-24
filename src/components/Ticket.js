764b6a91a31d051c832fb180c427410a0088769f
Riley Shimp <rilryshimp@gmail.com>
2022-04-24 09:07:46 -0700

    Add reusable form

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -4,8 +4,10 @@ import PropTypes from "prop-types";
 function Ticket(props){
   return (
     <React.Fragment>
-      <h3>{props.location} - {props.names}</h3>
-      <p><em>{props.issue}</em></p>
+      <div onClick = {() => props.whenTicketClicked(props.id)}>
+        <h3>{props.location} - {props.names}</h3>
+        <p><em>{props.issue}</em></p>
+      </div>
       <hr/>
     </React.Fragment>
   );
@@ -14,7 +16,9 @@ function Ticket(props){
 Ticket.propTypes = {
   names: PropTypes.string,
   location: PropTypes.string,
-  issue: PropTypes.string
+  issue: PropTypes.string,
+  id: PropTypes.string,
+  whenTicketClicked: PropTypes.func
 };
 
 export default Ticket;
